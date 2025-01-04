c632e1c2bd88b12ccb187025c0a4534587dc235f
sickdyd <roberto.reale.ja@gmail.com>
2025-01-04 18:14:33 +0900

    Remove model from player model

--- db/migrate/20250104061147_create_players.rb
+++ db/migrate/20250104061147_create_players.rb
@@ -3,7 +3,6 @@ class CreatePlayers < ActiveRecord::Migration[7.0]
     create_table :players do |t|
       t.string :steam_id
       t.string :name
-      t.string :model
 
       t.timestamps
     end
