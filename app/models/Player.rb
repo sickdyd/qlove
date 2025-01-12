e64cabee76f12597888e2ff31b7cb362bda5d8c5
sickdyd <roberto.reale.ja@gmail.com>
2025-01-12 11:34:24 +0900

    Improve player validation

--- app/models/Player.rb
+++ app/models/Player.rb
@@ -1,3 +1,5 @@
 class Player < ApplicationRecord
   has_many :stats, dependent: :destroy
+
+  validate :steam_id, presence: true, uniqueness: true
 end
--- db/migrate/20250104061147_create_players.rb
+++ db/migrate/20250104061147_create_players.rb
@@ -1,8 +1,8 @@
 class CreatePlayers < ActiveRecord::Migration[7.0]
   def change
     create_table :players do |t|
-      t.string :steam_id
-      t.string :name
+      t.string :steam_id, null: false
+      t.string :name, null: false, default: "UnnamedPlayer"
 
       t.timestamps
     end
