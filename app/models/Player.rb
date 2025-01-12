709de555bd84635add4d6739e6e4a5b4125dae9b
sickdyd <roberto.reale.ja@gmail.com>
2025-01-12 11:54:47 +0900

    Fix typo

--- app/models/Player.rb
+++ app/models/Player.rb
@@ -1,5 +1,5 @@
 class Player < ApplicationRecord
   has_many :stats, dependent: :destroy
 
-  validate :steam_id, presence: true, uniqueness: true
+  validates :steam_id, presence: true, uniqueness: true
 end
