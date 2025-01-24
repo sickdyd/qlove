ab2bfaff494c41a38aa4df71b86ceb58871e0841
sickdyd <roberto.reale.ja@gmail.com>
2025-01-24 21:28:39 +0900

    Fix wrong const reference

--- app/controllers/concerns/weapon_validatable.rb
+++ app/controllers/concerns/weapon_validatable.rb
@@ -12,9 +12,9 @@ module WeaponValidatable
 
     weapons = params[:weapons].split(',') if params[:weapons].is_a?(String) || []
 
-    invalid_weapons = weapons - AccuracyCalculatorService::ALL_WEAPONS
+    invalid_weapons = weapons - AccuracyStat::ALL_WEAPONS
     if invalid_weapons.any?
-      render json: { error: 'Invalid weapons', valid_weapons: AccuracyCalculatorService::ALL_WEAPONS }, status: :bad_request
+      render json: { error: 'Invalid weapons', valid_weapons: AccuracyStat::ALL_WEAPONS }, status: :bad_request
     end
   end
 end
