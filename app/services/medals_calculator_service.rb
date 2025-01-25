300283d7f0b38dc6b3f90b8c5f7699f9f9d72cda
sickdyd <roberto.reale.ja@gmail.com>
2025-01-25 11:58:40 +0900

    Fix issue with potential SQL injection

--- app/services/medals_calculator_service.rb
+++ app/services/medals_calculator_service.rb
@@ -8,18 +8,19 @@ class MedalsCalculatorService < BaseCalculatorService
   def time_filter_results
     query = Medal.joins(stat: :player).where('stats.created_at >= ?', start_time)
 
-    selected_medals = medals.map { |medal| "SUM(medals.#{medal}) as #{medal}" }
-    total_column = medals.map { |medal| "SUM(medals.#{medal})" }.join(" + ")
+    safe_medals = medals.map { |medal| ActiveRecord::Base.connection.quote_column_name(medal) }
+
+    selected_medals = safe_medals.map { |medal| "SUM(medals.#{medal}) AS #{medal}" }
+    total_column = safe_medals.map { |medal| "SUM(medals.#{medal})" }.join(' + ')
+
+    sanitized_sql = ActiveRecord::Base.sanitize_sql_array([
+      "players.name AS player_name, players.steam_id AS steam_id, #{total_column} AS total_medals, #{selected_medals.join(', ')}"
+    ])
 
     results = query
-      .select(
-        'players.name as player_name',
-        'players.steam_id as steam_id',
-        "#{total_column} AS total_medals",
-        *selected_medals
-      )
+      .select(Arel.sql(sanitized_sql))
       .group('players.id', 'players.name', 'players.steam_id')
-      .order('total_medals DESC')
+      .order(Arel.sql('total_medals DESC'))
       .limit(limit)
 
     results.map do |result|
@@ -29,8 +30,8 @@ class MedalsCalculatorService < BaseCalculatorService
         total_medals: result.total_medals.to_i
       }
 
-      medals.each do |medal|
-        player_data[medal.to_sym] = result[medal].to_i
+      safe_medals.each do |medal|
+        player_data[medal.delete('"').to_sym] = result[medal.delete('"')].to_i
       end
 
       player_data
