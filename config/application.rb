bed0c996e50416e5966a2254c6d13bcd6f73580f
sickdyd <roberto.reale.ja@gmail.com>
2025-01-25 12:09:39 +0900

    Upgrade to rails 8.0.1

--- config/application.rb
+++ config/application.rb
@@ -16,10 +16,6 @@ module Qlove
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
-    config.active_job.queue_adapter = :sidekiq
-
-    config.log_level = ENV.fetch('RAILS_LOG_LEVEL', 'error').to_sym
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
