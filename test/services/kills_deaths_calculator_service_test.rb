919e84146f8c78114b18d37eb2bddadffade4429
sickdyd <roberto.reale.ja@gmail.com>
2025-02-02 18:45:30 +0900

    Add kdr to kill deaths calculator service test

--- test/services/kills_deaths_calculator_service_test.rb
+++ test/services/kills_deaths_calculator_service_test.rb
@@ -16,22 +16,38 @@ class KillsDeathsCalculatorServiceTest < ActiveSupport::TestCase
 
     travel_to @current_time do
       3.times do
-        create(:stat, player: player1, created_at: within_the_day, kills: 10, deaths: 5)
-        create(:stat, player: player2, created_at: within_the_day, kills: 20, deaths: 10)
-        create(:stat, player: player3, created_at: within_the_day, kills: 30, deaths: 15)
-        create(:stat, player: player4, created_at: within_the_day, kills: 40, deaths: 20)
+        # player1: 36 kills, 9 deaths, kdr: 4
+        # player2: 24 kills, 21 deaths, kdr: 1.14
+        # player3: 54 kills, 33 deaths, kdr: 1.64
+        # player4: 45 kills, 12 deaths, kdr: 3.75
+        create(:stat, player: player1, created_at: within_the_day, kills: 12, deaths: 3)
+        create(:stat, player: player2, created_at: within_the_day, kills: 8, deaths: 7)
+        create(:stat, player: player3, created_at: within_the_day, kills: 18, deaths: 11)
+        create(:stat, player: player4, created_at: within_the_day, kills: 15, deaths: 4)
 
+        # player1: 45 kills, 30 deaths, (45 + 36) / (30 + 9) = 81 / 39 = 2.08
+        # player2: 75 kills, 45 deaths, (75 + 24) / (45 + 21) = 99 / 66 = 1.5
+        # player3: 105 kills, 60 deaths, (105 + 54) / (60 + 33) = 159 / 93 = 1.71
+        # player4: 135 kills, 75 deaths, (135 + 45) / (75 + 12) = 180 / 87 = 2.07
         create(:stat, player: player1, created_at: within_the_week, kills: 15, deaths: 10)
         create(:stat, player: player2, created_at: within_the_week, kills: 25, deaths: 15)
         create(:stat, player: player3, created_at: within_the_week, kills: 35, deaths: 20)
         create(:stat, player: player4, created_at: within_the_week, kills: 45, deaths: 25)
 
+        # player1: 81 + 60 = 141 kills, 39 + 45 = 84 deaths, 141 / 84 = 1.68
+        # player2: 99 + 90 = 189 kills, 66 + 60 = 126 deaths, 189 / 126 = 1.5
+        # player3: 159 + 120 = 279 kills, 93 + 75 = 168 deaths, 279 / 168 = 1.66
+        # player4: 180 + 150 = 330 kills, 87 + 90 = 177 deaths, 330 / 177 = 1.86
         create(:stat, player: player1, created_at: within_the_month, kills: 20, deaths: 15)
         create(:stat, player: player2, created_at: within_the_month, kills: 30, deaths: 20)
         create(:stat, player: player3, created_at: within_the_month, kills: 40, deaths: 25)
         create(:stat, player: player4, created_at: within_the_month, kills: 50, deaths: 30)
 
-        create(:stat, player: player1, created_at: all_time, kills: 25, deaths: 20)
+        # player1: 141 + 75 = 216 kills, 84 + 63 = 147 deaths, 216 / 147 = 1.47
+        # player2: 189 + 105 = 294 kills, 126 + 75 = 201 deaths, 294 / 201 = 1.46
+        # player3: 279 + 135 = 414 kills, 168 + 90 = 258 deaths, 414 / 258 = 1.6
+        # player4: 330 + 165 = 495 kills, 177 + 105 = 282 deaths, 495 / 282 = 1.75
+        create(:stat, player: player1, created_at: all_time, kills: 25, deaths: 21)
         create(:stat, player: player2, created_at: all_time, kills: 35, deaths: 25)
         create(:stat, player: player3, created_at: all_time, kills: 45, deaths: 30)
         create(:stat, player: player4, created_at: all_time, kills: 55, deaths: 35)
@@ -48,11 +64,11 @@ class KillsDeathsCalculatorServiceTest < ActiveSupport::TestCase
       @service = KillsDeathsCalculatorService.new(**kills_deaths_calculator_service_default_params)
       data = @service.leaderboard
 
-      assert_equal "Player Four", data.first.name
-      assert_equal 120, data.first.total_kills.to_i
+      assert_equal "Player Three", data.first.name
+      assert_equal 54, data.first.total_kills.to_i
 
-      assert_equal "Player Two", data.last.name
-      assert_equal 60, data.last.total_kills.to_i
+      assert_equal "Player One", data.last.name
+      assert_equal 36, data.last.total_kills.to_i
     end
   end
 
@@ -63,11 +79,26 @@ class KillsDeathsCalculatorServiceTest < ActiveSupport::TestCase
       )
       data = @service.leaderboard
 
-      assert_equal "Player Four", data.first.name
-      assert_equal 60, data.first.total_deaths.to_i
+      assert_equal "Player Three", data.first.name
+      assert_equal 33, data.first.total_deaths.to_i
 
-      assert_equal "Player Two", data.last.name
-      assert_equal 30, data.last.total_deaths.to_i
+      assert_equal "Player Four", data.last.name
+      assert_equal 12, data.last.total_deaths.to_i
+    end
+  end
+
+  test "daily kdr" do
+    travel_to @current_time do
+      @service = KillsDeathsCalculatorService.new(**kills_deaths_calculator_service_default_params
+        .merge(sort_by: KillsDeathsCalculatorService::KILL_DEATH_RATIO_COLUMN)
+      )
+      data = @service.leaderboard
+
+      assert_equal "Player One", data.first.name
+      assert_equal 4, data.first.kill_death_ratio.to_f
+
+      assert_equal "Player Three", data.last.name
+      assert_equal 1.64, data.last.kill_death_ratio.to_f
     end
   end
 
@@ -79,10 +110,10 @@ class KillsDeathsCalculatorServiceTest < ActiveSupport::TestCase
       data = @service.leaderboard
 
       assert_equal "Player Four", data.first.name
-      assert_equal 255, data.first.total_kills.to_i
+      assert_equal 180, data.first.total_kills.to_i
 
       assert_equal "Player Two", data.last.name
-      assert_equal 135, data.last.total_kills.to_i
+      assert_equal 99, data.last.total_kills.to_i
     end
   end
 
@@ -96,11 +127,29 @@ class KillsDeathsCalculatorServiceTest < ActiveSupport::TestCase
       )
       data = @service.leaderboard
 
-      assert_equal "Player Four", data.first.name
-      assert_equal 135, data.first.total_deaths.to_i
+      assert_equal "Player Three", data.first.name
+      assert_equal 93, data.first.total_deaths.to_i
 
       assert_equal "Player Two", data.last.name
-      assert_equal 75, data.last.total_deaths.to_i
+      assert_equal 66, data.last.total_deaths.to_i
+    end
+  end
+
+  test "weekly kdr" do
+    travel_to @current_time do
+      @service = KillsDeathsCalculatorService.new(**kills_deaths_calculator_service_default_params
+        .merge(
+          sort_by: KillsDeathsCalculatorService::KILL_DEATH_RATIO_COLUMN,
+          time_filter: "week"
+        )
+      )
+      data = @service.leaderboard
+
+      assert_equal "Player One", data.first.name
+      assert_equal 2.08, data.first.kill_death_ratio.to_f
+
+      assert_equal "Player Three", data.last.name
+      assert_equal 1.71, data.last.kill_death_ratio.to_f
     end
   end
 
@@ -112,10 +161,10 @@ class KillsDeathsCalculatorServiceTest < ActiveSupport::TestCase
       data = @service.leaderboard
 
       assert_equal "Player Four", data.first.name
-      assert_equal 405, data.first.total_kills.to_i
+      assert_equal 330, data.first.total_kills.to_i
 
       assert_equal "Player Two", data.last.name
-      assert_equal 225, data.last.total_kills.to_i
+      assert_equal 189, data.last.total_kills.to_i
     end
   end
 
@@ -130,12 +179,32 @@ class KillsDeathsCalculatorServiceTest < ActiveSupport::TestCase
       data = @service.leaderboard
 
       assert_equal "Player Four", data.first.name
-      assert_equal 225, data.first.total_deaths.to_i
+      assert_equal 177, data.first.total_deaths.to_i
 
       assert_equal "Player Two", data.last.name
-      assert_equal 135, data.last.total_deaths.to_i
+      assert_equal 126, data.last.total_deaths.to_i
     end
   end
+
+  test "monthly kdr" do
+    travel_to @current_time do
+      @service = KillsDeathsCalculatorService.new(**kills_deaths_calculator_service_default_params
+        .merge(
+          sort_by: KillsDeathsCalculatorService::KILL_DEATH_RATIO_COLUMN,
+          time_filter: "month"
+        )
+      )
+      data = @service.leaderboard
+
+      assert_equal "Player Four", data.first.name
+      assert_equal 1.86, data.first.kill_death_ratio.to_f
+
+      assert_equal "Player Three", data.last.name
+      assert_equal 1.66, data.last.kill_death_ratio.to_f
+    end
+  end
+
+
 
   test "all time kills" do
     travel_to @current_time do
@@ -145,10 +214,10 @@ class KillsDeathsCalculatorServiceTest < ActiveSupport::TestCase
       data = @service.leaderboard
 
       assert_equal "Player Four", data.first.name
-      assert_equal 570, data.first.total_kills.to_i
+      assert_equal 495, data.first.total_kills.to_i
 
       assert_equal "Player Two", data.last.name
-      assert_equal 330, data.last.total_kills.to_i
+      assert_equal 294, data.last.total_kills.to_i
     end
   end
 
@@ -163,10 +232,28 @@ class KillsDeathsCalculatorServiceTest < ActiveSupport::TestCase
       data = @service.leaderboard
 
       assert_equal "Player Four", data.first.name
-      assert_equal 330, data.first.total_deaths.to_i
+      assert_equal 282, data.first.total_deaths.to_i
 
       assert_equal "Player Two", data.last.name
-      assert_equal 210, data.last.total_deaths.to_i
+      assert_equal 201, data.last.total_deaths.to_i
+    end
+  end
+
+  test "all time kdr" do
+    travel_to @current_time do
+      @service = KillsDeathsCalculatorService.new(**kills_deaths_calculator_service_default_params
+        .merge(
+          sort_by: KillsDeathsCalculatorService::KILL_DEATH_RATIO_COLUMN,
+          time_filter: "all_time"
+        )
+      )
+      data = @service.leaderboard
+
+      assert_equal "Player Four", data.first.name
+      assert_equal 1.76, data.first.kill_death_ratio.to_f
+
+      assert_equal "Player One", data.last.name
+      assert_equal 1.47, data.last.kill_death_ratio.to_f
     end
   end
 
