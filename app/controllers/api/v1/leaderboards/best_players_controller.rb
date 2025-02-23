709c087dec57d538343ff9b45825b0e9270a7ef5
sickdyd <roberto.reale.ja@gmail.com>
2025-02-23 12:01:33 +0900

    Fix best players leaderboard

--- app/controllers/api/v1/leaderboards/best_players_controller.rb
+++ app/controllers/api/v1/leaderboards/best_players_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::Leaderboards::BestPlayersController < Api::V1::BaseController
   def show
-    render json: { data: BestPlayerCalculatorService.best_players(**best_players_params.to_h.symbolize_keys) }
+    params_with_sort_by = best_players_params.to_h.symbolize_keys.merge(sort_by: BestPlayerCalculatorService::SORT_BY_COLUMN)
+    data = BestPlayerCalculatorService.new(**params_with_sort_by).leaderboard
+    render json: { data: data }
   end
 
   private
