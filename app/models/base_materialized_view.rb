7c0d729a44162f73b3c3d756fc946e0c4181ee63
sickdyd <roberto.reale.ja@gmail.com>
2025-05-03 16:53:23 +0900

    Remove unused methods

--- app/models/base_materialized_view.rb
+++ app/models/base_materialized_view.rb
@@ -20,15 +20,4 @@ class BaseMaterializedView < ApplicationRecord
       .order("#{sort_by} DESC")
       .limit(limit)
   end
-
-  private
-
-  def self.to_table(data:, headers:, time_filter:, sort_by:)
-    title = table_title(time_filter: time_filter, sort_by: sort_by)
-    TabletizeService.new(title: title, data: data, headers: headers).table
-  end
-
-  def self.table_title(time_filter:, sort_by:)
-    time_filter == "all_time" ? "All Time #{sort_by.titleize}" : "#{sort_by.titleize} for the #{time_filter}"
-  end
 end
