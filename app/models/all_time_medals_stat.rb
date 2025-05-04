4aba7d4dabf6fd3982263574724cb6dd3ebdbf60
sickdyd <roberto.reale.ja@gmail.com>
2025-05-04 10:47:23 +0900

    Remove redundant consts

--- app/models/all_time_medals_stat.rb
+++ app/models/all_time_medals_stat.rb
@@ -2,6 +2,4 @@ class AllTimeMedalsStat < BaseMaterializedView
   self.primary_key = :id
 
   belongs_to :player, foreign_key: :player_id, primary_key: :id
-
-  TOTAL_MEDALS_COLUMN = "total_medals".freeze
 end
